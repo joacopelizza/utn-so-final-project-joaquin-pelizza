2621f99aebf16c1601f0bd1ced379bfac141e43c
joaquin <joaquinpelizza@gmail.com>
2025-07-08 21:09:36 -0300

    Proyecto final de Docker completado

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const db = require("./db");
 
 // Define express app
 const app = express();
-const port = 3000;
+const port = 4000;
 
 // Middleware to parse JSON requests
 app.use(express.json());
@@ -17,6 +17,32 @@ app.get("/api/students", async (req, res) => {
   } catch (err) {
     console.error(err);
     res.status(500).send("DB error");
+  }
+});
+app.get("/api/greet", (req, res) => {
+  const name = req.query.name || "invitado";
+  res.send(`Hola, ${name}!`);
+});
+
+app.post("/students", async (req, res) => {
+  const { name } = req.body;
+
+  if (!name) {
+    return res.status(400).json({ error: "El nombre es requerido" });
+  }
+
+  try {
+    const result = await db.query(
+      "INSERT INTO students (name) VALUES ($1) RETURNING id, name",
+      [name]
+    );
+
+    const student = result.rows[0];
+    res.status(201).json(student);
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Error al agregar estudiante" });
   }
 });
 
